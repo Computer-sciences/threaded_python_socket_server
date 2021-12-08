fa60eb63c873db58f1682da9ad6ffd1bf70dde33
Eric Dee <atallbirdmaker@protonmail.com>
2021-12-07 17:27:29 -0700

    Preparing code for https socket handling

--- server.py
+++ server.py
@@ -12,11 +12,12 @@ import socket
 #       add data cleansing methods
 #       handle threads
 #       add a server restart method
+#       add server shutdown and restart commands
 
-data_format = 'ascii'
+ascii_format = 'ascii'
 
-all_connections = []
-all_aliases = []
+all_ascii_connections = []
+all_ascii_aliases = []
 
 def broadcast(broadcast_group, message):
     for connection in broadcast_group:
@@ -25,49 +26,49 @@ def broadcast(broadcast_group, message):
         except:
             continue
 
-def remove_connection(connection):
-    index = all_connections.index(connection)
-    alias = all_aliases[index]
-    broadcast(all_connections, "~ connection ~ {} is no longer connected.\n".format(alias).encode(data_format))
-    all_connections.remove(connection)
-    all_aliases.remove(alias)
+def remove_ascii_connection(connection):
+    index = all_ascii_connections.index(connection)
+    alias = all_ascii_aliases[index]
+    broadcast(all_ascii_connections, "~ connection ~ {} is no longer connected.\n".format(alias).encode(ascii_format))
+    all_ascii_connections.remove(connection)
+    all_ascii_aliases.remove(alias)
     connection.close()
 
 def close_all_connections():
-    for connection in all_connections:
+    for connection in all_ascii_connections:
         connection.close()
-    all_connections[:]
-    all_aliases[:]
+    all_ascii_connections[:]
+    all_ascii_aliases[:]
 
 ################
 # Instruction headers:
 
-close_instruction = 'close'.encode(data_format)
-server_stats_instruction = 'how many'.encode(data_format)
+close_instruction = 'close'.encode(ascii_format)
+server_stats_instruction = 'how many'.encode(ascii_format)
 
 ################
 
-def an_individual_connection(connection, connection_address):
-    index = all_connections.index(connection)
-    alias = all_aliases[index].encode(data_format)
+def an_individual_ascii_connection(connection, connection_address):
+    index = all_ascii_connections.index(connection)
+    alias = all_ascii_aliases[index].encode(ascii_format)
     connected = True
     while connected:
         try:
             message = connection.recv(1024)
             broadcast_message = alias + b": " + message + b"\n"
-            print("\n{} said: {}".format(connection_address, message))
-            print("{} was sent as a full broadcast".format(broadcast_message))
-            broadcast(all_connections, broadcast_message)
+            print("{} said: {}".format(connection_address, message))
+            print("{} was sent as a full broadcast\n".format(broadcast_message))
+            broadcast(all_ascii_connections, broadcast_message)
             if message == close_instruction:
-                user_goodbye = "~ external ~ {} closed their connection.\n".format(alias).encode(data_format)
+                user_goodbye = "~ external ~ {} closed their connection.\n".format(alias).encode(ascii_format)
                 print(user_goodbye)
-                broadcast(all_connections, user_goodbye)
+                broadcast(all_ascii_connections, user_goodbye)
                 connected = False
             if message == server_stats_instruction:
-                connection.send("~ count ~ There is/are {} active connection/s.\n".format(threading.activeCount() - 1).encode(data_format))
+                connection.send("~ count ~ There is/are {} active connection/s.\n".format(threading.activeCount() - 1).encode(ascii_format))
         except:
             connected = False
-    remove_connection(connection)
+    remove_ascii_connection(connection)
 
 
 
@@ -75,32 +76,39 @@ def an_individual_connection(connection, connection_address):
 ip_server = socket.gethostbyname(socket.gethostname())
 port_server = 10000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((ip_server, port_server))
-server.listen(256)
-print("~ listen ~ The server is listening on {}:{}".format(ip_server, port_server))
+ascii_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ascii_server.bind((ip_server, port_server))
 
 
 
 
 def start_routing():
+
+    ascii_server.listen(16)
+    print("\n~ listen ~ The ascii server is listening on {}:{}\n".format(ip_server, port_server))
+
+    close_all_connections()
+
     while True:
-        client, address = server.accept()
-        print("A computer successfully connected from {}.".format(str(address)))
+        ascii_client, ascii_client_address = ascii_server.accept()
+        print("A computer successfully connected from {}.".format(str(ascii_client_address)))
 
-        client.send('send_an_alias'.encode(data_format))
-        alias = client.recv(1024).decode(data_format)
+        ascii_client.send('send_an_alias'.encode(ascii_format))
+        alias = ascii_client.recv(1024).decode(ascii_format)
 
-        print("New connection alias is {}".format(alias))
-        all_aliases.append(alias)
-        all_connections.append(client)
+        print("New connection alias is {}\n".format(alias))
+        all_ascii_aliases.append(alias)
+        all_ascii_connections.append(ascii_client)
 
-        broadcast(all_connections, "{} joined the chat.\n".format(alias).encode(data_format))
-        client.send('You have successfully connected.\n'.encode(data_format))
+        broadcast(all_ascii_connections, "{} joined the chat.\n".format(alias).encode(ascii_format))
+        ascii_client.send('You have successfully connected.\n'.encode(ascii_format))
 
         # Add address to args
-        thread = threading.Thread(target=an_individual_connection, args=(client, address))
-        thread.start()
+        ascii_thread = threading.Thread(target=an_individual_ascii_connection, args=(ascii_client, ascii_client_address))
+        ascii_thread.start()
 
-close_all_connections()
+
+
+
+#
 start_routing()
